1618f1fa41980598e500f9f1aa66e0e988762879
PANDURI DHANA SRI RAM <dsriram916@gmail.com>
2026-04-24 14:32:23 +0530

    Update WebSocketConfig.java

--- Websocket_demo/src/main/java/com/AML3A/Websocket_demo/config/WebSocketConfig.java
+++ Websocket_demo/src/main/java/com/AML3A/Websocket_demo/config/WebSocketConfig.java
@@ -16,8 +16,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("*")
-                .withSockJS();
+registry.addEndpoint("/ws")
+        .setAllowedOriginPatterns(
+            "https://chatapp-pearl-theta.vercel.app"
+        )
+        .withSockJS();
     }
 }
